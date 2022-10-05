eb4b89e160eac5af43602049f89e6b5d0e463e0a
Igor Fernandes <igormedfer@gmail.com>
2022-10-05 15:26:40 -0300

    fetch data into the tableView

--- NetflixClone/Controllers/ComingSoonViewController.swift
+++ NetflixClone/Controllers/ComingSoonViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ComingSoon: UIViewController {
+    var titles: [MoviesAndTVShows] = [MoviesAndTVShows]()
     lazy var tableView: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -17,22 +18,39 @@ class ComingSoon: UIViewController {
         tableView.dataSource = self
         
         view.addSubview(tableView)
+        fetchUpcoming()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let array):
+                self.titles = array
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 extension ComingSoon: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Text"
+        cell.textLabel?.text = titles[indexPath.row].original_title ?? titles[indexPath.row].original_name ?? "Unknown"
         return cell
     }
 }
