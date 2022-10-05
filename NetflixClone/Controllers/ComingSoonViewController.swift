4ae2c5d9b8b4faa2d72023de135d66ac4bc85baa
Igor Fernandes <igormedfer@gmail.com>
2022-10-05 15:14:49 -0300

    added upcoming tableView

--- NetflixClone/Controllers/ComingSoonViewController.swift
+++ NetflixClone/Controllers/ComingSoonViewController.swift
@@ -1,8 +1,38 @@
 import UIKit
 
 class ComingSoon: UIViewController {
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Upcoming"
         self.view.backgroundColor = .defaultBackgroundColor
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.largeTitleDisplayMode = .always
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
+
+extension ComingSoon: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Text"
+        return cell
     }
 }
